0a41f4446f14cfdffec05efdd4f986e6e2ad585b
DiasOrazbaev <orazbaevdev@gmail.com>
2022-09-03 23:37:01 +0600

    H4 completed

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,81 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Head   *ListItem
+	Tail   *ListItem
+	length int
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.Head
+}
+
+func (l *list) Back() *ListItem {
+	return l.Tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	node := &ListItem{Value: v}
+	if l.Head == nil {
+		l.Head = node
+		l.Tail = node
+	} else {
+		node.Next = l.Head
+		l.Head.Prev = node
+		l.Head = node
+	}
+	l.length++
+	return node
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	node := &ListItem{Value: v}
+	if l.Tail == nil {
+		l.Head = node
+		l.Tail = node
+	} else {
+		l.Tail.Next = node
+		node.Prev = l.Tail
+		l.Tail = node
+	}
+	l.length++
+	return node
+}
+
+func (l *list) Remove(i *ListItem) {
+	switch i {
+	case l.Head:
+		i.Next.Prev = nil
+		l.Head = i.Next
+	case l.Tail:
+		l.Tail = i.Prev
+		l.Tail.Next = nil
+		i.Prev = nil
+	default:
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+
+		i.Next = nil
+		i.Prev = nil
+	}
+	l.length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.Head.Prev = i
+	i.Next = l.Head
+	l.Head = i
 }
 
 func NewList() List {
-	return new(list)
+	return &list{
+		Head:   nil,
+		Tail:   nil,
+		length: 0,
+	}
 }
